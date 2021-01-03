2b507a1ad00323a15426207bdbec4df7551b0408
Eliel Taskinen <AA3737@student.jamk.fi>
2021-01-03 23:30:05 +0200

    Started on 3a

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -28,7 +28,12 @@ import Data.List
 --  maxBy head   [1,2,3] [4,5]  ==>  [4,5]
 
 maxBy :: (a -> Int) -> a -> a -> a
-maxBy measure a b = todo
+maxBy measure a b = 
+    if measure a >= measure b
+        then a
+    else
+        b
+
 
 ------------------------------------------------------------------------------
 -- Ex 2: implement the function mapMaybe that takes a function and a
@@ -40,7 +45,8 @@ maxBy measure a b = todo
 --   mapMaybe length (Just "abc") ==> Just 3
 
 mapMaybe :: (a -> b) -> Maybe a -> Maybe b
-mapMaybe f x = todo
+mapMaybe f Nothing = Nothing
+mapMaybe f (Just a) = Just (f a)
 
 ------------------------------------------------------------------------------
 -- Ex 3: implement the function mapMaybe2 that works like mapMaybe
@@ -54,7 +60,9 @@ mapMaybe f x = todo
 --   mapMaybe2 div (Just 6) Nothing   ==>  Nothing
 
 mapMaybe2 :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
-mapMaybe2 f x y = todo
+mapMaybe2 f Nothing _ = Nothing
+mapMaybe2 f _ Nothing = Nothing
+mapMaybe2 f (Just x) (Just y) = Just (f x y)
 
 ------------------------------------------------------------------------------
 -- Ex 4: define the functions firstHalf and palindrome so that
@@ -76,9 +84,21 @@ mapMaybe2 f x y = todo
 palindromeHalfs :: [String] -> [String]
 palindromeHalfs xs = map firstHalf (filter palindrome xs)
 
-firstHalf = todo
+firstHalf' x 0 = ""
+firstHalf' [] _ = []
+firstHalf' x i = (head x): firstHalf' (tail x) (i-1) 
 
-palindrome = todo
+firstHalf xs = 
+    if odd (length xs)
+        then firstHalf' xs ((length xs) `div` 2 + 1)
+    else
+        firstHalf' xs ((length xs) `div` 2)
+
+palindrome word = 
+    if reverse word == word
+        then True
+    else
+        False
 
 ------------------------------------------------------------------------------
 -- Ex 5: Implement a function capitalize that takes in a string and
@@ -96,7 +116,9 @@ palindrome = todo
 --   capitalize "goodbye cruel world" ==> "Goodbye Cruel World"
 
 capitalize :: String -> String
-capitalize = todo
+capitalize sentence = unwords [capitalizeFirst x | x<-words sentence]
+
+capitalizeFirst (x:xs) = toUpper x : xs
 
 ------------------------------------------------------------------------------
 -- Ex 6: powers k max should return all the powers of k that are less
@@ -113,8 +135,13 @@ capitalize = todo
 --   * the function takeWhile
 
 powers :: Int -> Int -> [Int]
-powers k max = todo
+powers k max = powers' k max 0
 
+powers' k max pot = 
+    if k^pot <= max
+        then k^pot : powers' k max (pot + 1)
+    else
+        []
 ------------------------------------------------------------------------------
 -- Ex 7: implement a functional while loop. While should be a function
 -- that takes a checking function, an updating function, and an
@@ -136,7 +163,11 @@ powers k max = todo
 --     ==> Avvt
 
 while :: (a->Bool) -> (a->a) -> a -> a
-while check update value = todo
+while check update value = 
+    if not (check value)
+        then value
+    else
+        while check update (update value)
 
 ------------------------------------------------------------------------------
 -- Ex 8: another version of a while loop. This time, the check
@@ -154,6 +185,12 @@ while check update value = todo
 
 whileRight :: (a -> Either b a) -> a -> b
 whileRight f x = todo
+--whileRight f x = 
+--    if isRight (f x)
+--        then whileRight f (f x)
+--    else
+ --       x
+
 
 -- for the whileRight examples:
 -- step k x doubles x if it's less than k
